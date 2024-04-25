38358ed5e9256dce12fde0da329ca977d6188f48
Michael <mhgballantyne@gmail.com>
2024-04-25 15:18:31 +0100

    We did need It
    + New test class

--- tests/src/com/skloch/game/tests/AssetTests.java
+++ tests/src/com/skloch/game/tests/AssetTests.java
@@ -1,4 +1,5 @@
 package com.skloch.game.tests;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.skloch.game.Energy;
@@ -7,18 +8,13 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(GdxTestRunner.class)
-public class EnergyTests {
+public class AssetTests {
     @Test
-    public void testDecreaseEnergy() {
-        assertEquals(20, 20);
-
-    }
-
-    @Test
-    public void testTest(){
-        assertEquals(true, true);
+    public void testIfFileExists() {
+        System.out.println("000000000000000000");
+        assertTrue("The asset for player exists", Gdx.files.internal("assets/title.png").exists());
     }
 }
-
