c9bff83f6c0fb0370ffef2b0a42cd3fa1e0e1aa2
Zachary <160025727+ZackIsInTheBox@users.noreply.github.com>
2024-05-06 23:51:08 +0100

    Map update 6

---
+++ assets/East Campus/Tilemaps/Bus.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Bus" tilewidth="24" tileheight="24" tilecount="24" columns="6">
+ <image source="../Textures/Public Bus.png" width="150" height="106"/>
+</tileset>
