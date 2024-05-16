98efc614ccce2dc1fd8f6dafdc3ce2875b5eb05e
Zachary <160025727+ZackIsInTheBox@users.noreply.github.com>
2024-05-17 00:16:26 +0100

    Added city

---
+++ assets/East Campus/Tilemaps/city_foreground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="city_foreground" tilewidth="24" tileheight="24" tilecount="4240" columns="80">
+ <image source="../Textures/city_foreground.png" width="1920" height="1280"/>
+</tileset>
